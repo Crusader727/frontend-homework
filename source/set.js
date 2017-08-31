3aff05c41557ba8f5b7de0c33a8d3a3208a3785a
Crusader727 <rubenhov97@gmail.com>
2017-08-31 17:25:09 +0300

    removed comments

--- source/set.js
+++ source/set.js
@@ -1,19 +1,5 @@
 'use strict'
-/*
-function set(object, field, value) {
-    let arr = field.split('.');
-    arr.shift();
-    let objectCopy = object;
-    while (typeof objectCopy === "object" && arr.length > 1) {
-        let buffer = arr.shift();
-        if (typeof objectCopy[buffer] === "undefined") {
-            objectCopy[buffer] = {};
-        }
-        objectCopy = objectCopy[buffer];
-    }
-    objectCopy[arr.shift()] = value;
-    return object;
-};*/
+
 function set(object, field, value) {
     let arr = field.split('.');
     arr = arr.slice(1);
