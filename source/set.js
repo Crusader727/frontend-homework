ac87f01b85cb06adb2de09ec7e25eebdf8985b1b
Crusader727 <rubenhov97@gmail.com>
2017-08-31 17:12:56 +0300

    shift to foreach

--- source/set.js
+++ source/set.js
@@ -1,5 +1,5 @@
 'use strict'
-
+/*
 function set(object, field, value) {
     let arr = field.split('.');
     arr.shift();
@@ -12,5 +12,22 @@ function set(object, field, value) {
         objectCopy = objectCopy[buffer];
     }
     objectCopy[arr.shift()] = value;
+    return object;
+};*/
+function set(object, field, value) {
+    let arr = field.split('.');
+    arr = arr.slice(1);
+    let objectCopy = object;
+    let length = arr.length;
+    arr.forEach(function(item, i, arr) {
+        if (typeof objectCopy === "object" && i < length - 1) {
+            if (typeof objectCopy[item] === "undefined") {
+                objectCopy[item] = {};
+            }
+            objectCopy = objectCopy[item];
+        }
+    }, this);
+    objectCopy[arr[length - 1]] = value;
+
     return object;
 };
